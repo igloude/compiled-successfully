dc2c0e595846548f52b321c94ad320f2f8717fd0
Ian Gloude <igloude@gmail.com>
2019-08-05 10:15:17 -0500

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-46161882-3`,
       },
     },
     `gatsby-plugin-feed`,
