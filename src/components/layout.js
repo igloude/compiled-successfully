ca4804168a6a865e5ff63c0fc6e685d6b6fdf749
Ian Gloude <igloude@gmail.com>
2019-08-06 08:37:49 -0500

    add blink animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ class Layout extends React.Component {
       header = (
         <h1>
           <Link
+            className="blink"
             style={{
               boxShadow: `none`,
               textDecoration: `none`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="All posts" className="blink" />
         <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
