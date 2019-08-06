a291481ebc510c09bb717a2892e3a20a0e693003
Ian Gloude <igloude@gmail.com>
2019-08-06 09:05:39 -0500

    fixed breakpoints and site container

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,14 +43,7 @@ class Layout extends React.Component {
       )
     }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <main className="container">
         <header>{header}</header>
         <main>{children}</main>
         <footer>
@@ -60,7 +53,7 @@ class Layout extends React.Component {
             Gatsby
           </a>
         </footer>
-      </div>
+      </main>
     )
   }
 }
